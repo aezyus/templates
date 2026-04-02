af88d1552ec944c32f601689c62de646f30bb5e5
Ayu. <aezyus1@gmail.com>
2026-04-02 22:21:29 +0530

    feat: overload vector read

--- main.cpp
+++ main.cpp
@@ -12,62 +12,57 @@ using pii = pair<int,int>;
 #define f first
 #define s second
 
-// Loop macros
-#define rep(i,a,b) for(int i=(a); i<(b); i++)
-#define rrep(i,a,b) for(int i=(a); i>=(b); i--)
-
 // Toggles
-#define Multitests   // comment for single test
-// #define LOCAL     // uncomment for debug
-
-template<class T>
-ostream& operator<<(ostream& os, const vector<T>& v) {
-  for (const auto &x : v) os << x << ' ';
-  return os;
-}
+#define Multitests // comment for single test
+// #define LOCAL // uncomment for debug
 
 #define m1(x) template<class T, class... U> inline void x(T&& a, U&&... b)
 #define m2(x) (void)initializer_list<int>{((x std::forward<U>(b)),0)...}
+
+m1(re) { cin >> std::forward<T>(a); m2(cin >>); }
+
+template<class T>
+void re(vector<T>& v) {
+    for (auto &x : v) re(x);
+}
+
+template<class A, class B>
+void re(pair<A,B>& p) {
+    re(p.first, p.second);
+}
+
 m1(pr) { cout << std::forward<T>(a); m2(cout << ' ' <<); cout << '\n'; }
-m1(re) { cin  >> std::forward<T>(a); m2(cin  >>); }
 
 #ifdef LOCAL
 m1(db) { cerr << std::forward<T>(a); m2(cerr << ' ' <<); cerr << '\n'; }
 #define dbg(...) cerr << "[" << #__VA_ARGS__ << "] = ", db(__VA_ARGS__)
-
-template<class T>
-void dbg_vec(const vector<T>& v){
-  cerr << "[ ";
-  for(auto &x: v) cerr << x << " ";
-  cerr << "]\n";
-}
 #else
 #define dbg(...)
 #endif
 
 void setIO(string name = "") {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-  if (!name.empty()) {
-    freopen((name + ".in").c_str(), "r", stdin);
-    freopen((name + ".out").c_str(), "w", stdout);
-  }
+    if (!name.empty()) {
+        freopen((name + ".in").c_str(), "r", stdin);
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
 }
-
 void solve() {
-  
+
 }
 
 int main() {
-  setIO();
+    setIO();
 
 #ifdef Multitests
-  int T; cin >> T;
-  while (T--) solve();
+    int T; 
+    re(T);
+    while (T--) solve();
 #else
-  solve();
+    solve();
 #endif
 
-  return 0;
+    return 0;
 }
